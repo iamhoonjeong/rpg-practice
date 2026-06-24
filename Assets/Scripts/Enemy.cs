481fd1885386b0b02ecaba89f952093516edbf4c
Hoon Jeong <iamhoonjeong@gmail.com>
2026-06-23 19:03:00 -0700

    Implemented basic enemy behavior

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class Enemy : Entity
+{
+    public Enemy_IdleState idleState;
+    public Enemy_MoveState moveState;
+    public Enemy_AttackState attackState;
+
+    [Header("Movement details")]
+    public float idleTime = 2f;
+    public float moveSpeed = 1.4f;
+    [Range(0, 2)]
+    public float moveAnimSpeedMultiplier = 1;
+}
---
+++ Assets/Scripts/StateMachine/EnemyState.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class EnemyState : EntityState
+{
+    protected Enemy enemy;
+
+    public EnemyState(Enemy enemy, StateMachine stateMachine, string animBoolName) : base(stateMachine, animBoolName)
+    {
+        this.enemy = enemy;
+
+        rb = enemy.rb;
+        anim = enemy.anim;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (Input.GetKeyDown(KeyCode.R))
+            stateMachine.ChangeState(enemy.attackState);
+        anim.SetFloat("moveAnimSpeedMultiplier", enemy.moveAnimSpeedMultiplier);
+    }
+}
